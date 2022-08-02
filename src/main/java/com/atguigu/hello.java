1b34bed64ec19ce97f531db9a3b36d55b0109439
liliangwei <1312818920@qq.com>
2022-08-02 23:08:52 +0800

    ver1.0 --init

---
+++ src/main/java/com/atguigu/hello.java
@@ -0,0 +1,8 @@
+package com.atguigu;
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello github");
+        System.out.println("hello github");
+    }
+}
